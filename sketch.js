e8a24a9e7cbe26bcf0727ed207a183b4e4ddcf7a
laksh970 <103823214+laksh970@users.noreply.github.com>
2022-12-11 17:14:01 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -58,17 +58,17 @@ function draw() {
   
   //code to reset the background
 
-   //if(path.x > height ){
-    // path.x = height/2;
-  // }
+ /*  if(path.x > height ){
+     path.x = height/2;
+   }*/
 
-   //if(path.y > height ){
-    // path.x = height/2;
-  // }
+  /* if(path.y > height ){
+     path.x = height/2;
+   }*/
 
    //if(path.x > height ){
-    // path.y = height;
-  // }
+     //path.y = height;
+   //}
 
    if(path.y > height ){
      path.y = height/2;
